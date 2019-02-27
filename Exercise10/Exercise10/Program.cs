c0e05b763cb062fb176754cb1b1bb3bca86cda66
Lukas Roessner <30021032@student.toiohomai.ac.nz>
2019-02-28 10:17:43 +1300

    finished1

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    public class animal
+    {
+        public int ID { get; }
+        public string Name { get; }
+        public string Type { get; }
+        public int Age { get; }
+
+        public animal(int id, string name, string type, int age)
+        {
+            ID = id;
+            Name = name;
+            Type = type;
+            Age = age;
+        }
+
+        public string returnAnimal()
+        {
+            return "ID: " + ID + ", Name: " + Name + ", Type: " + Type + ", Age: " + Age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many animals do you want to create: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            animal[] animals = new animal[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                int id = i + 1;
+                Console.WriteLine("--------" + id + "--------");
+                Console.WriteLine("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Type: ");
+                string type = Console.ReadLine();
+                Console.WriteLine("Age: ");
+                int age = Convert.ToInt32(Console.ReadLine());
+                animals[i] = new animal(id, name, type, age);
+            }
+
+            Console.WriteLine("--Animals Created--");
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine(animals[i].returnAnimal());
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -24,21 +24,46 @@ namespace Exercise9
                     Console.WriteLine("Variable 1: " + optionSwap(A, B)[0]);
                     Console.WriteLine("Variable 2: " + optionSwap(A, B)[1]);
                 }
-                else if (input == "Bye")
-                {
+                else if (input == "multiply")
+                {  
+                    Console.WriteLine("First Number: ");
+                    string num1Str = Console.ReadLine();
 
+                    Console.WriteLine("First Second: ");
+                    string num2Str = Console.ReadLine();
+
+                    Console.WriteLine(num1Str + " x " + num2Str + " = " + multiply(num1Str, num2Str));
                 }
-                else if (input == "Bye")
+                else if (input == "times table")
                 {
+                    Console.WriteLine("Input number: ");
+                    string inputStr = Console.ReadLine();
 
+                    int num = 0;
+                    int x = 1;
+                    do
+                    {
+                        Console.WriteLine(optionTimesTbl(inputStr)[num]);
+                        x += 1;
+                        num += 1;
+                    } while (x != 11);
+                    
                 }
                 else if (input == "end")
                 {
                     i = +1;
                 }
+                else if (input == "help")
+                {
+                    Console.WriteLine(">swap");
+                    Console.WriteLine(">multiply");
+                    Console.WriteLine(">times table");
+                    Console.WriteLine(">end");
+                    Console.WriteLine(">help");
+                }
                 else
                 {
-                    Console.WriteLine("Unknown Command");
+                    Console.WriteLine("Unknown Command. Type 'end' to emd the program");
                 }
             }
         }
@@ -56,6 +81,40 @@ namespace Exercise9
             T temp = var1;
             var1 = var2;
             var2 = temp;
+        }
+
+        public static int multiply(string num1Str, string num2Str)
+        {
+            int num1;
+            int num2;
+            Int32.TryParse(num1Str, out num1);
+            Int32.TryParse(num2Str, out num2);
+
+            int result = num1 * num2;
+            return result;
+        }
+
+        public static string[] optionTimesTbl(string inputStr)
+        {
+            double input;
+            input = Convert.ToDouble(inputStr);
+            string[] answer = new string[10];
+            int x = 0;
+            int num = 1;
+            do
+            {
+                answer[x] = multiplyTable(input, num);
+                x += 1;
+                num += 1;
+            } while (x != 10);
+
+            return answer;
+        }
+        static string multiplyTable(Double main, Double num)
+        {
+            Double answr = main * num;
+            string line = main + " x " + num + " = " + answr;
+            return line;
         }
     }
 }
