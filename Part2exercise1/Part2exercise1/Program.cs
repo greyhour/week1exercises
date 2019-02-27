28c801bc87f4f064da25d1f410bc1f4ba86b524e
Lukas Roessner <30021032@student.toiohomai.ac.nz>
2019-02-28 11:01:38 +1300

    part2started

---
+++ Part2exercise1/Part2exercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int Min = 1000;
+            int Max = 9999;
+            Random randNum = new Random();
+            int[] ranArray = Enumerable
+                .Repeat(0, input)
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            Array.Sort(ranArray);
+
+            foreach (var item in ranArray)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
