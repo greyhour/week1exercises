3756115669b8f8f63998bd8639e7d66f5a3f0509
Lukas Roessner <30021032@student.toiohomai.ac.nz>
2019-02-28 11:08:17 +1300

    part2exe2

---
+++ Part2exercise2/Part2exercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Min = 1;
+            int Max = 100;
+            Random randNum = new Random();
+            int[] ranArray = Enumerable
+                .Repeat(0, 100)
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            foreach (var item in ranArray)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            int sum = ranArray.Sum();
+
+            Console.WriteLine("The sum is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
