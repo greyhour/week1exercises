36cc2997e0a09d36a9f7a9fb9c4df9c28841867d
Lukas Roessner <30021032@student.toiohomai.ac.nz>
2019-03-01 08:57:25 +1300

    part2exe3,4

---
+++ Part2exercise3/Part2exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2exercise3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Names divided by commas: ");
+            string data = Console.ReadLine();
+
+            string[] names = data.Split(',');
+
+            Array.Sort(names);
+
+            string[] finalNames = new string[names.Length];
+
+            names.CopyTo(finalNames, 0);
+
+            foreach (string word in finalNames)
+            {
+                Console.WriteLine("WORD: " + word);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Part2exercise4/Part2exercise4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 10 numbers seperated by commas: ");
+            string input = Console.ReadLine();
+
+            string[] numbersStr = input.Split(',');
+
+            int[] numbers = Array.ConvertAll(numbersStr, int.Parse);
+
+            Array.Sort(numbers);
+
+            int highest = numbers[numbers.Length - 1];
+            int lowest = numbers[0];
+            int average = 0;
+
+            int x = 1;
+            foreach (int num in numbers)
+            {
+                average = num + average;
+                    x += 1;
+            }
+            average = average / x;
+            
+
+            Console.WriteLine("The highest number is: " + highest);
+            Console.WriteLine("The lowest number is: " + lowest);
+            Console.WriteLine("The average number is: " + average);
+
+            Console.ReadLine();
+        }
+    }
+}
